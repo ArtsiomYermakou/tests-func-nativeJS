020de7f3f826208745c7f370d1146b0b960cc201
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-06-28 16:43:44 +0300

    add 02.test/object-testing

---
+++ src/02/02.ts
@@ -0,0 +1,55 @@
+type localCityType = {
+    title: string
+    countryTitle: string
+}
+type AddressType = {
+    streetTitle: string
+    city: localCityType
+}
+type TechnologiesType = {
+    id: number
+    title: string
+}
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechnologiesType>
+}
+
+const student:StudentType = {
+    id: 1,
+    name: "Artem",
+    age: 21,
+    isActive: true,
+    address: {
+        streetTitle: "Sviridova",
+        city:{
+            title: "Gomel",
+            countryTitle: "Belarus",
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "JS"
+        },
+        {
+            id: 3,
+            title: "React"
+        },
+    ]
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
+
+
